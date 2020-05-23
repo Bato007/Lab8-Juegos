555e916ef4f7c601ab18ef290e12790b8f3752ff
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-05-23 16:08:05 -0600

    Initial commit

---
+++ Assets/Code/MyLight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyLight : MonoBehaviour
+{
+    public Light myLight;
+    private bool isOn = false;
+    private float intensityS, velocidad = 5, amplitud = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intensityS = myLight.intensity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOn)
+        {
+            myLight.intensity = intensityS + (Mathf.Sin(Time.time * velocidad) * amplitud);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isOn = !isOn;
+        myLight.enabled = isOn;
+    }
+
+}
